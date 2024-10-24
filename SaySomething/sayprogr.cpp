f72339952c3716c874427be788ee238e9323f7d0
IldarMustafinCode <ildarmn1994@gmail.com>
2024-10-24 23:44:56 +0300

    branch1

---
+++ SaySomething/sayprogr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int mainSayProgr()
+{   
+    char something;
+    std::cout << "Input something: ";
+    std::cin >> something;
+    std::cout << "Input weight: ";
+    std::cin >> something;
+    std::cout << "your word: " << something << std::endl;
+}
