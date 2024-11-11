e09d969fd7822266ba30cfbbc6b8f4dbe66c4404
ильдар мустафин <ildarmn1994@gmail.com>
2024-11-11 20:10:25 +0300

    test4

--- Tests/mainTest.cpp
+++ Tests/mainTest.cpp
@@ -17,14 +17,15 @@ TEST(eigenTest, vector)
 	const double tol = 1E-10;
 	Eigen::ArrayXf ar = Eigen::ArrayXf::Random(10);
 
-	ASSERT_NEAR(SumArray(ar), ar.sum(), tol);
-	// ASSERT_NEAR(std::sin(0), 1, tol);
+	// ASSERT_NEAR(SumArray(ar), ar.sum(), tol);
+	ASSERT_NEAR(0, 0, tol);
 	//  GTEST_ASSERT_TRUE(SumArray(ar), ar.sum()+1, 1e-6);
 }
 
 int main(int argc, char **argv)
 {
 
+	float t=0;
 
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
