c90aa4b7b23476d51d830d7ce9445b9b1461d154
ильдар мустафин <ildarmn1994@gmail.com>
2024-11-11 14:35:44 +0300

    test2

--- Tests/mainTest.cpp
+++ Tests/mainTest.cpp
@@ -1,29 +1,32 @@
 #include <gtest/gtest.h>
 
-#include <Eigen/Core>
-#include <Eigen/Dense>
+#include "SomeFunctions/SumArray.hpp"
 
-#include "SaySomething/eigenTest.h"
+TEST(eigenTest, sss)
+{
+	
+	const double tol = 1E-10;
+	Eigen::ArrayXf ar = Eigen::ArrayXf::Random(10);
 
-
+	ASSERT_NEAR(1, 1, tol);
+	//  GTEST_ASSERT_TRUE(SumArray(ar), ar.sum()+1, 1e-6);
+}
 
 TEST(eigenTest, vector)
 {
-
+	const double tol = 1E-10;
 	Eigen::ArrayXf ar = Eigen::ArrayXf::Random(10);
 
-
-	 GTEST_ASSERT_TRUE(SumArray(ar), ar.sum(), 1e-6);
-
+	// ASSERT_NEAR(SumArray(ar), ar.sum(), tol);
+	ASSERT_NEAR(std::sin(0), 1, tol);
+	//  GTEST_ASSERT_TRUE(SumArray(ar), ar.sum()+1, 1e-6);
 }
-
-
-
 
 int main(int argc, char **argv)
 {
 
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-
+	std::cout << "wowowowowoowwoowowowowowow" << std::endl;
+	float t = 0;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
